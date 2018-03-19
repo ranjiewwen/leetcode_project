24b5defbdfa309b1d264f70803c48fda828a7029
ranjiewwen <532685734@qq.com>
2018-03-19 16:02:49 +0800

    add 60. Permutation Sequence

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -5805,7 +5805,51 @@ public:
 	}
 };
 
+// 60. Permutation Sequence
+class Solution_60 {
+public:
+	string getPermutation(int n, int k) {
 
+		vector<int> vec;
+		for (int i = 0; i < n;i++)
+		{
+			vec.push_back(i + 1);
+		}
+		
+		for (int i = 0; i < k-1;i++)
+		{
+			next_permutation(vec.begin(), vec.end());
+		}
+
+		string res;
+		for (int i = 0; i < vec.size();i++)
+		{
+			char temp = vec[i] + '0';
+			res.push_back(temp);
+		}
+		
+		return res;
+	}
+
+	string getPermutation_ref(int n, int k) {
+		vector<int> permutation(n + 1, 1);
+		for (int i = 1; i <= n; ++i) {
+			permutation[i] = permutation[i - 1] * i;
+		}
+		vector<char> digits = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+		int num = n - 1;
+		string res;
+		while (num) {
+			int t = (k - 1) / (permutation[num--]);
+			k = k - t * permutation[num + 1];
+			res.push_back(digits[t]);
+			digits.erase(digits.begin() + t);
+		}
+		res.push_back(digits[k - 1]);
+		return res;
+	}
+
+};
 
 #define cin infile
 #include <fstream>
@@ -5813,6 +5857,8 @@ public:
 int main()
 {   
 
+	Solution_60 su_60;
+	su_60.getPermutation_ref(2,1);
 
 	Solution_54 su_54;
 	//vector<vector<int>> vec(3, vector<int>(3, 1));
